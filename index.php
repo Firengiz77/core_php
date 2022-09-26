826e1c968d32c10fb7dafff95bf8561ef64c6695
Firengiz77 <firengizsariyeva77@gmail.com>
2022-09-26 17:13:07 +0400

    include require ferqleri file cagirma meselesi

--- index.php
+++ index.php
@@ -61,20 +61,78 @@
 // echo Nl2br($yazi2);  alty alta yazilmani gorur
 // $yazi3 = str_replace('My','Menim',$yazi); evez edir 
 // echo $yazi3;
-$data = 'Admin@1234';
-$data = explode('@',$data);
+// $data = 'Admin@1234';
+// $data = explode('@',$data);
 // echo $data[0];   @den evvelkileri gosteir
 // echo $data[1]  @den sonrakilari gosterir eger 2ci @ => onda $data[3] yazariq
-$login= $data[0];
-$parol = $data[1];
-echo "$login and $parol";
+// $login= $data[0];
+// $parol = $data[1];
+// echo "$login and $parol";
 // ders 14
 
 
 
+// calculator
+
+
+
+// if($_POST){
+//     // eger form a tiklanilibsa demekdir
+//  $a = trim($_POST['reqema']);
+//  $b = trim($_POST['reqemb']);
+//  $emeliyyat = $_POST['emeliyyat'];
+// //  !$a ve ya empty($a)
+//  if(!$a or empty($b)){
+//     echo ' Xanalari doldurun';
+//     die();
+//  }
+// // is_numeric burda reqem olmasi demekdi ! ile eksini yoxlayiriq
+//  if(!is_numeric($a) or !is_numeric($b))
+// echo " sadece reqem ola biler";
+// };
+// if(strlen($a) > 5){
+//     echo "cox simvol daxil etdiniz";
+// };
+
+// if($emeliyyat == 1){
+//      $cavab =$a+$b;
+// };
+// if($emeliyyat ==2){
+//     $cavab =$a-$b;
+// };
+// if($emeliyyat ==3){
+//     $cavab =$a*$b;
+// };
+// if($emeliyyat == 4){
+//     $cavab =$a/$b;
+// };
+// // burda switch da ola biler
+
+// echo $cavab;
+
+
+// include "cagir.php";
+// bu basqa filedir $a ise cagir.php icindeki a filesidir
+// echo $a;
+
+// echo " Firngiz Sariyeva";
+// include "cagir.php";
+// bir nece defe cagirila biler ve emeliyyat her defe cagirildiqda isheyer sehv yazilsa error verer ama novbeti funksiya isleyer
+// include_once "cagir.php";
+// include_once ise bir nece defe yazilsada yalniz 1 defe isleyer
+// require "cagir.php";
+// require_once "cagir.php"; bir defe ucundu
+// require ise sehv yazilsa die olur avtomativ ve daha islemir novbeti funksiyalar
 
 
 ?>
+
+
+
+
+
+
+
 
 <!-- <form action="gonder.php" method="get">
 ad: <br>
@@ -120,7 +178,22 @@ Reqem b: <br>
 
 <input type="submit" value="Hesabla">
 </form> -->
+<!-- 
 
+<form action="index.php" method="post">
+reqem A <br>
+<input type="text" name="reqema">
+reqem b <br>
+<input type="text" name="reqemb">
+emeliiyat : <br>
+<select name="emeliyyat" id="">
+    <option value="1">toplama</option>
+    <option value="2">cixma</option>
+    <option value="3">vurma</option>
+    <option value="4">bolme</option>
+</select>
 
+<input type="submit" value="hesable">
+</form> -->
 
 
